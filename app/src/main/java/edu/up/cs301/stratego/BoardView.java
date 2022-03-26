7cf87c79c6b2c35f0c71fe9afe69b240155aad4e
Aurora <kathryn.aurora.w@gmail.com>
2022-03-25 17:32:13 -0700

    Trying to figure out putting pieces on the board

--- app/src/main/java/edu/up/cs301/stratego/BoardView.java
+++ app/src/main/java/edu/up/cs301/stratego/BoardView.java
@@ -2,6 +2,8 @@ package edu.up.cs301.stratego;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
 import android.widget.ImageView;
@@ -11,6 +13,7 @@ import edu.up.cs301.game.R;
 
 public class BoardView extends AnimationSurface {
 
+    private StrategoGameState gameState;
 
     public BoardView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -24,7 +27,7 @@ public class BoardView extends AnimationSurface {
      *
      * method to get the Unit onto the GUI board
      *
-     * @param canvas  the drawing space for the Unit
+     * @param canvas the drawing space for the Unit
      */
     public void drawMe(Canvas canvas){
         //TODO: need a rect somewhere with constant/final dimensions
@@ -32,6 +35,23 @@ public class BoardView extends AnimationSurface {
         //TODO: maybe xy locations to be drawn in? no clue how we'll get those
         //TODO: death by ifs for each rank's visual aspect?
 
+        //dimensions are 60dp wide per piece
+
+        Paint paint = new Paint();
+        paint.setColor(0x00000000);
+        paint.setStyle(Paint.Style.STROKE);
+
+        Paint paint2 = new Paint();
+        paint2.setColor(Color.BLUE);
+        paint2.setStyle(Paint.Style.FILL);
+
+        canvas.drawRect(0,0,60,60, paint);
+        //Rectangles not in dp I think
+
+
+
+
     }
+
 
 }//BoardView
