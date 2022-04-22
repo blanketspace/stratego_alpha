c4deb96db3ac66e9572d3262289b2dc0aa155135
Aurora <kathryn.aurora.w@gmail.com>
2022-04-21 18:15:31 -0700

    Idk

---
+++ app/src/main/java/edu/up/cs301/stratego/actions/EndAction.java
@@ -0,0 +1,13 @@
+package edu.up.cs301.stratego.actions;
+
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+
+public class EndAction extends GameAction {
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public EndAction(GamePlayer player) { super(player); }
+}
