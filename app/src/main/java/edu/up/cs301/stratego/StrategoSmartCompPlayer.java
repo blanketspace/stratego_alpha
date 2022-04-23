9f5051e5246850df8033dd60300052f23e51d5f6
Aurora <kathryn.aurora.w@gmail.com>
2022-04-22 21:42:13 -0700

    smartstrat2 method is created

--- app/src/main/java/edu/up/cs301/stratego/StrategoSmartCompPlayer.java
+++ app/src/main/java/edu/up/cs301/stratego/StrategoSmartCompPlayer.java
@@ -232,18 +232,18 @@ public class StrategoSmartCompPlayer extends GameComputerPlayer {
     public void setUpStrategy(int chosenStrat) {
         switch (chosenStrat) { //We have possible setups from a world champion stratego player
             case 0:
-                //smth to fill the ranks properly
+               //invoke smartStrat1
                 break;
             case 1:
-                //smth
+                //invoke smartStrat2
                 break;
             case 2:
-                //smth
+                //Random setup?
                 break;
         }
-    } //Do we *need* multiple setups? theyre kinda long...
+    }
 
-    private void smartStrat(StrategoGameState gameState) { //UNTESTED
+    private void smartStrat1(StrategoGameState gameState) { //UNTESTED
         ArrayList<Unit> p1Troops = gameState.getP1Troops();
         int i = 0, j = 0; //i is left/right, j is up/down
         int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0; //how many times each piece has placed
@@ -442,7 +442,211 @@ public class StrategoSmartCompPlayer extends GameComputerPlayer {
             } //X and Y for placePiece from dumbComp setup method
             gameState.placePiece(playerNum, troop, i * troop.UNIT_WIDTH, (j + 6) * troop.UNIT_HEIGHT);
         }
-    } //end smartStrat
+    } //end smartStrat1
+
+    private void smartStrat2(StrategoGameState gameState) { //UNTESTED
+        ArrayList<Unit> p1Troops = gameState.getP1Troops();
+        int i = 0, j = 0; //i is left/right, j is up/down
+        int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0; //how many times each piece has placed
+
+        //this loop assigns each Unit a location on the board based on the Unit's rank
+        for (Unit troop: p1Troops) {
+            troop.setDead(false);
+            switch (troop.getRank()) {
+                case 1:
+                    i = 6;
+                    j = 2;
+                    break;
+                case 2:
+                    j = 0;
+                    switch (a) {
+                        case 0:
+                            i = 1;
+                            break;
+                        case 1:
+                            i = 2;
+                            break;
+                        case 2:
+                            i = 3;
+                            break;
+                        case 3:
+                            i = 5;
+                            break;
+                        case 4:
+                            i = 4;
+                            j = 1;
+                            break;
+                        case 5:
+                            i = 8;
+                            j = 1;
+                            break;
+                        case 6:
+                            i = 0;
+                            j = 2;
+                            break;
+                        case 7:
+                            i = 8;
+                            j = 2;
+                            break;
+                    }
+                    a++;
+                    break; //end case 2
+                case 3:
+                    j = 3;
+                    switch (b) {
+                        case 0:
+                            i = 0;
+                            break;
+                        case 1:
+                            i = 6;
+                            break;
+                        case 2:
+                            i = 7;
+                            break;
+                        case 3:
+                            i = 8;
+                            break;
+                        case 4:
+                            i = 6;
+                            j = 0;
+                            break;
+                    }
+                    b++;
+                    break; //end case 3
+                case 4:
+                    switch (c) {
+                        case 0:
+                            i = 3;
+                            j = 1;
+                            break;
+                        case 1:
+                            i = 9;
+                            j = 1;
+                            break;
+                        case 2:
+                            i = 2;
+                            j = 2;
+                            break;
+                        case 3:
+                            i = 5;
+                            j = 3;
+                            break;
+                    }
+                    c++;
+                    break; //end case 4
+                case 5:
+                    switch (d) {
+                        case 0:
+                            i = 7;
+                            j = 0;
+                            break;
+                        case 1:
+                            i = 0;
+                            j = 1;
+                            break;
+                        case 2:
+                            i = 1;
+                            j = 2;
+                            break;
+                        case 3:
+                            i = 4;
+                            j = 2;
+                            break;
+                    }
+                    d++;
+                    break; //end case 5
+                case 6:
+                    j = 0;
+                    switch (e) {
+                        case 0:
+                            i = 0;
+                            break;
+                        case 1:
+                            i = 4;
+                            break;
+                        case 2:
+                            i = 8;
+                            break;
+                        case 3:
+                            i = 5;
+                            j = 2;
+                            break;
+                    }
+                    e++;
+                    break; //end case 6
+                case 7:
+                    switch (f) {
+                        case 0:
+                            i = 7;
+                            j = 2;
+                            break;
+                        case 1:
+                            i = 9;
+                            j = 2;
+                            break;
+                        case 2:
+                            i = 7;
+                            j = 3;
+                            break;
+                    }
+                    f++;
+                    break; //end case 7
+                case 8:
+                    switch (g) {
+                        case 0:
+                            i = 6;
+                            j = 1;
+                            break;
+                        case 1:
+                            i = 7;
+                            j = 1;
+                            break;
+                    }
+                    g++;
+                    break; //end case 8
+                case 9:
+                    i = 5;
+                    j = 1;
+                    break; //end case 9
+                case 10:
+                    i = 9;
+                    j = 0;
+                    break; //end case 10
+                case 11:
+                    j = 3;
+                    switch (h) {
+                        case 0:
+                            i = 1;
+                            break;
+                        case 1:
+                            i = 2;
+                            break;
+                        case 2:
+                            i = 4;
+                            break;
+                        case 3:
+                            i = 3;
+                            j = 2;
+                            break;
+                        case 4:
+                            i = 1;
+                            j = 1;
+                            break;
+                        case 5:
+                            i = 2;
+                            j = 1;
+                            break;
+                    }
+                    h++;
+                    break; //end case 11
+                case 12:
+                    i = 3;
+                    j = 3;
+                    break; //end case 12
+            } //X and Y for placePiece from dumbComp setup method
+            gameState.placePiece(playerNum, troop, i * troop.UNIT_WIDTH, (j + 6) * troop.UNIT_HEIGHT);
+        }
+    } //end smartStrat2
 
     public void choosePiece() {} //returns a Unit, not void
 
